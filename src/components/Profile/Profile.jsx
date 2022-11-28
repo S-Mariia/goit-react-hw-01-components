c0ebb3a7e71a1450982a07e07e136a2222e92f5e
S-Mariia <105200661+S-Mariia@users.noreply.github.com>
2022-11-28 13:40:34 +0200

    did profile

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,39 @@
+import css from 'components/Profile/Profile.module.css';
+
+export const Profile = ({
+  user: {
+    username,
+    tag,
+    location,
+    avatar,
+    stats: { followers, views, likes },
+  },
+}) => {
+
+    console.log('css',css);
+  return (
+      <div className={css.profile}>
+      <div className={css.description}>
+        <img src={avatar} alt={username} className={css.avatar} width="200" height="200"/>
+        <p className={css.name}>{username}</p>
+        <p className={css.tag}>@{tag}</p>
+        <p className={css.location}>{location}</p>
+      </div>
+
+      <ul className={css.stats}>
+        <li className={css.statsItem}>
+          <span className={css.label}>Followers</span>
+          <span className={css.quantity}>{followers}</span>
+        </li>
+        <li className={css.statsItem}>
+          <span className={css.label}>Views</span>
+          <span className={css.quantity}>{views}</span>
+        </li>
+        <li className={css.statsItem}>
+          <span className={css.label}>Likes</span>
+          <span className={css.quantity}>{likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
